205dcc5e9df375f37234b00c1501fd6546282dcf
cld-vasconcelos <c.vasconcelos@runtime-revolution.com>
2023-05-11 15:22:57 +0100

    AWS basic static website deployment using Cloudfront and S3

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "b" {
 
   tags = {
     Name        = "My bucket"
-    Environment = "Dev"
+    Environment = "development"
   }
 }
 
@@ -15,45 +15,91 @@ resource "aws_s3_bucket_ownership_controls" "b_ownership_controls" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "b_public_access_block" {
+  bucket = aws_s3_bucket.b.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
+
+
 resource "aws_s3_bucket_acl" "b_acl" {
-  depends_on = [aws_s3_bucket_ownership_controls.b_ownership_controls]
+  depends_on = [
+    aws_s3_bucket_ownership_controls.b_ownership_controls,
+    aws_s3_bucket_public_access_block.b_public_access_block
+  ]
 
   bucket = aws_s3_bucket.b.id
-  acl    = "private"
+  acl    = "public-read"
+
+}
+
+resource "aws_s3_bucket_policy" "b_policy" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.b_public_access_block
+  ]
+
+  bucket = aws_s3_bucket.b.id
+  policy = <<EOF
+{
+ "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": ["s3:GetObject"],
+        "Resource": "arn:aws:s3:::thereisnowaythisbucketalreadyexists/*"
+      }
+    ]
+}
+EOF
+}
+
+resource "aws_s3_bucket_website_configuration" "b_website" {
+  bucket = aws_s3_bucket.b.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
 }
 
 # Cloudfront
 locals {
-  s3_origin_id = "myS3Origin"
+  s3_origin_id = "s3-my-webapp.example.com"
 }
 
-resource "aws_cloudfront_origin_access_control" "s3__origin_access_control" {
-  name                              = "example"
-  description                       = "Example Policy"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "s3-my-webapp.example.com"
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.s3__origin_access_control.id
-    origin_id                = local.s3_origin_id
+    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "Some comment"
+  comment             = "my-cloudfront"
   default_root_object = "index.html"
 
-  logging_config {
-    include_cookies = false
-    bucket          = aws_s3_bucket.b.bucket_domain_name
-    prefix          = "cf-logs/"
-  }
+  #logging_config {
+  #  include_cookies = false
+  #  bucket          = "mylogs.s3.amazonaws.com"
+  #  prefix          = "myprefix"
+  #}
 
-  aliases = null
+  #aliases = ["mywebsite.example.com", "s3-static-web-dev.example.com"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -74,6 +120,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     max_ttl                = 86400
   }
 
+  price_class = "PriceClass_200"
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
@@ -82,7 +130,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   tags = {
-    Environment = "Dev"
+    Environment = "development"
+    Name        = "my-tag"
   }
 
   viewer_certificate {
