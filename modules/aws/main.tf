1ec41f5fc01e1db56cc1f0a6049f81d7a8451a02
cld-vasconcelos <c.vasconcelos@runtime-revolution.com>
2023-05-11 12:07:52 +0100

    Added AWS Cloudfront resource

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -21,3 +21,71 @@ resource "aws_s3_bucket_acl" "b_acl" {
   bucket = aws_s3_bucket.b.id
   acl    = "private"
 }
+
+# Cloudfront
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_origin_access_control" "s3__origin_access_control" {
+  name                              = "example"
+  description                       = "Example Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3__origin_access_control.id
+    origin_id                = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = aws_s3_bucket.b.bucket_domain_name
+    prefix          = "cf-logs/"
+  }
+
+  aliases = null
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
