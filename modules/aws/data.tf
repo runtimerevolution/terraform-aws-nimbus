eaae5ed4cf5513400c38a470a7d822fbca81172a
cld-vasconcelos <c.vasconcelos@runtime-revolution.com>
2023-05-12 14:53:35 +0100

    AWS S3 bucket access strengthen

---
+++ modules/aws/data.tf
@@ -0,0 +1,11 @@
+data "aws_iam_policy_document" "iam_policy_document" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3_bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+}
--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -1,6 +1,9 @@
+# CloudFront Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {}
+
 # S3 bucket
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = var.aws_s3_bucket_name
+  bucket        = var.aws_s3_bucket_name
   force_destroy = true
 }
 
@@ -14,12 +17,11 @@ resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership_controls" {
 resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
   bucket = aws_s3_bucket.s3_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
+  block_public_acls       = true
+  block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = false
+  restrict_public_buckets = true
 }
-
 
 resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   depends_on = [
@@ -28,29 +30,13 @@ resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   ]
 
   bucket = aws_s3_bucket.s3_bucket.id
-  acl    = "public-read"
+  acl    = "private"
 
 }
 
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
-  depends_on = [
-    aws_s3_bucket_public_access_block.s3_bucket_public_access_block
-  ]
-
   bucket = aws_s3_bucket.s3_bucket.id
-  policy = <<EOF
-{
- "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": ["s3:GetObject"],
-        "Resource": "arn:aws:s3:::${var.aws_s3_bucket_name}/*"
-      }
-    ]
-}
-EOF
+  policy = data.aws_iam_policy_document.s3_bucket_policy.json
 }
 
 # Cloudfront
@@ -58,6 +44,10 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   origin {
     domain_name = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
     origin_id   = var.aws_cloudfront_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
   }
 
   enabled             = true
