b4022fd3733f9abbcf0169b7063c5cd844432917
cld-vasconcelos <c.vasconcelos@runtime-revolution.com>
2023-05-29 10:39:39 +0100

    Added descriptions to variables

--- modules/aws/modules/network/variables.tf
+++ modules/aws/modules/network/variables.tf
@@ -1,28 +1,29 @@
 variable "solution_name" {
-  type = string
+  type        = string
+  description = "Name of the solution"
 }
 
 variable "vpc_cidr_block" {
-  type = string
+  type        = string
   description = "The IPv4 CIDR block for the VPC."
 }
 
 variable "public_subnets_count" {
-  type = number
+  type        = number
   description = "The number of public subnets to create."
 }
 
 variable "private_subnets_count" {
-  type = number
+  type        = number
   description = "The number of private subnets to create."
 }
 
 variable "from_port" {
-  type = number
+  type        = number
   description = "Start port for requests to the network."
 }
 
 variable "to_port" {
-  type = number
+  type        = number
   description = "End port for requests to the network."
 }
