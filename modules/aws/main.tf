7b932bb477a660df6a84200ec39f15cc64e8c26c
cld-vasconcelos <c.vasconcelos@runtime-revolution.com>
2023-06-12 09:50:26 +0100

    Added comment to bastion host module

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -92,6 +92,9 @@ module "databases" {
   databases         = var.databases
 }
 
+# -----------------------------------------------------------------------------
+# Bastion host to access resources in private subnets through a SSH tunnel
+# -----------------------------------------------------------------------------
 module "bastion_host" {
   count = var.enable_bastion_host ? 1 : 0
 
