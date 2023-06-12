456ccfd872106b25d956ea3e7e2653e1e7203432
cld-vasconcelos <c.vasconcelos@runtime-revolution.com>
2023-06-12 14:17:15 +0100

    Removed load balancer Route53 entry creation

--- modules/aws/modules/load_balancer/main.tf
+++ modules/aws/modules/load_balancer/main.tf
@@ -3,20 +3,3 @@ resource "aws_lb" "lb" {
   subnets         = var.subnets_ids
   security_groups = [var.security_group_id]
 }
-
-# -----------------------------------------------------------------------------
-# Create a alias for the load balancer in Route53
-# -----------------------------------------------------------------------------
-resource "aws_route53_record" "alias" {
-  count = var.enable_custom_domain ? 1 : 0
-
-  zone_id = var.route53_zone_id
-  name    = "lb.${var.domain}"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.lb.dns_name
-    zone_id                = aws_lb.lb.zone_id
-    evaluate_target_health = true
-  }
-}
