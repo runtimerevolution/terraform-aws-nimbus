99d8ac622e0890ef241072784d70543e9a53d2d0
cld-vasconcelos <c.vasconcelos@runtime-revolution.com>
2023-06-30 12:27:45 +0100

    Database secret string change

--- modules/aws/modules/databases/secrets.tf
+++ modules/aws/modules/databases/secrets.tf
@@ -9,11 +9,9 @@ resource "aws_secretsmanager_secret" "db_credentials" {
 resource "aws_secretsmanager_secret_version" "bastion_ssh_private_key" {
   count = var.enable_secrets_manager ? length(var.databases) : 0
 
-  secret_id     = aws_secretsmanager_secret.db_credentials[count.index].id
-  secret_string = <<EOF
-  {
-    "username": "${aws_db_instance.instance[count.index].username}",
-    "password": "${aws_db_instance.instance[count.index].password}"
-  }
-EOF
+  secret_id = aws_secretsmanager_secret.db_credentials[count.index].id
+  secret_string = jsonencode({
+    username = "${aws_db_instance.instance[count.index].username}"
+    password = "${aws_db_instance.instance[count.index].password}"
+  })
 }
